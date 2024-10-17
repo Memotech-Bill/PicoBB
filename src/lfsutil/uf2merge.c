50d04af0eb65bd96e85c6b4fe7fed0e63551728d
Memotech Bill <64212746+Memotech-Bill@users.noreply.github.com>
2024-10-17 20:47:53 +0100

    Enhancements to uf2merge

--- src/lfsutil/uf2merge.c
+++ src/lfsutil/uf2merge.c
@@ -33,6 +33,17 @@ typedef struct
     uint32_t    iAddrEnd;
     } FILEINFO;
 
+void help (const char *psProg)
+    {
+    fprintf (stderr, "Usage: %s [-h] [-e] [-f | -i] [-p pad] -o output.uf2 input1.uf2 input2.uf2...\n", psProg);
+    fprintf (stderr, "       -h - Display this help and exit\n");
+    fprintf (stderr, "       -e - Erase (fill) gaps between inputs\n");
+    fprintf (stderr, "       -f - Set family of subsequent UF2 files to match first\n");
+    fprintf (stderr, "       -i - Ignore (preserve) different family settings\n");
+    fprintf (stderr, "       -p - Set pad byte (default 0xFF)\n");
+    exit (1);
+    }
+
 int main (int nArg, const char *psArg[])
     {
     FILEINFO *fi;
@@ -40,44 +51,94 @@ int main (int nArg, const char *psArg[])
     FILEINFO **pfs;
     FILE *fOut;
     FILE *fIn;
+    const char *psOut = NULL;
     uint32_t iBuff[BLK_SIZE / 4];
     uint32_t iFamily = 0;
     uint32_t nBlkSize = 0;
+    uint32_t uPad = 0xFF;
     int iErr = 0;
-    int nFIn = 2;
+    int nFIn = 0;
+    int bEra = 0;
     int bIgn = 0;
     int bFix = 0;
-    if ( nArg < 3 )
+    int i = 1;
+    while (i < nArg)
         {
-        fprintf (stderr, "Usage: %s [-f | -i] output.uf2 input1.uf2 input2.uf2...\n", psArg[0]);
-        fprintf (stderr, "       -f - Set family of subsequent UF2 files to match first\n");
-        fprintf (stderr, "       -i - Ignore (preserve) different family settings\n");
-        exit (1);
+        if (! strcmp (psArg[i], "-h"))
+            {
+            help (psArg[0]);
+            }
+        else if (! strcmp (psArg[i], "-e"))
+            {
+            bEra = 1;
+            }
+        else if (! strcmp (psArg[i], "-f"))
+            {
+            bFix = 1;
+            bIgn = 0;
+            }
+        else if (! strcmp (psArg[i], "-i"))
+            {
+            bFix = 0;
+            bIgn = 1;
+            }
+        else if (! strcmp (psArg[i], "-o"))
+            {
+            ++i;
+            if (i >= nArg) help (psArg[0]);
+            psOut = psArg[i];
+            }
+        else if (! strcmp (psArg[i], "-p"))
+            {
+            ++i;
+            if (i >= nArg) help (psArg[0]);
+            sscanf (psArg[i], "%i", &uPad);
+            }
+        else
+            {
+            ++nFIn;
+            }
+        ++i;
         }
-    if (! strcmp (psArg[1], "-f"))
+    if (psOut == NULL)
         {
-        bFix = 1;
-        nFIn = 3;
+        fprintf (stderr, "No output file.\n");
+        help (psArg[0]);
         }
-    else if (! strcmp (psArg[1], "-i"))
+    if (nFIn == 0)
         {
-        bIgn = 1;
-        nFIn = 3;
+        fprintf (stderr, "No input fils.\n");
+        help (psArg[0]);
         }
-    fi = (FILEINFO *) malloc ((nArg - nFIn) * sizeof (FILEINFO));
-    pfs = (FILEINFO **) malloc ((nArg - nFIn) * sizeof (FILEINFO *));
+    fi = (FILEINFO *) malloc (nFIn * sizeof (FILEINFO));
+    pfs = (FILEINFO **) malloc (nFIn * sizeof (FILEINFO *));
     if (( fi == NULL ) || ( pfs == NULL ))
         {
         fprintf (stderr, "Memory allocation failure\n");
         exit (1);
         }
+    i = 1;
     pfi = fi;
-    for (int iArg = nFIn; iArg < nArg; ++iArg)
+    while (i < nArg)
         {
-        fIn = fopen (psArg[iArg], "rb");
+        if (psArg[i][0] == '-')
+            {
+            if (strchr ("op", psArg[i][1])) ++i;
+            }
+        else
+            {
+            pfi->iArg = i;
+            ++pfi;
+            }
+        ++i;
+        }
+    pfi = fi;
+    for (i = 0; i < nFIn; ++i)
+        {
+        fIn = fopen (psArg[pfi->iArg], "rb");
         if ( fIn == NULL )
             {
-            fprintf (stderr, "Unable to open input file \"%s\"\n", psArg[iArg]);
+            fprintf (stderr, "Unable to open input file \"%s\"\n", psArg[pfi->iArg]);
             iErr = 1;
             break;
             }
@@ -85,17 +146,17 @@ int main (int nArg, const char *psArg[])
         size_t nByte = fread (iBuff, 1, BLK_SIZE, fIn);
         if ( nByte < BLK_SIZE )
             {
-            fprintf (stderr, "Read error on \"%s\"\n", psArg[iArg]);
+            fprintf (stderr, "Read error on \"%s\"\n", psArg[pfi->iArg]);
             iErr = 1;
             break;
             }
         if ( ( iBuff[0] != MAGIC_0 ) || ( iBuff[1] != MAGIC_1 ) || ( iBuff[BLK_END] != MAGIC_2 ) )
             {
-            fprintf (stderr, "Block %d of \"%s\" is not valid UF2 data", iBlkIn, psArg[iArg]);
+            fprintf (stderr, "Block %d of \"%s\" is not valid UF2 data", iBlkIn, psArg[pfi->iArg]);
             iErr = 1;
             break;
             }
-        if ( iArg == nFIn )
+        if ( i == 0 )
             {
             nBlkSize = iBuff[4];
             if ( iBuff[2] & FLG_FAMILY )
@@ -107,7 +168,7 @@ int main (int nArg, const char *psArg[])
             {
             if ( iBuff[4] != nBlkSize )
                 {
-                fprintf (stderr, "Inconsistent block size in \"%s\": %d %d\n", psArg[iArg], nBlkSize, iBuff[4]);
+                fprintf (stderr, "Inconsistent block size in \"%s\": %d %d\n", psArg[pfi->iArg], nBlkSize, iBuff[4]);
                 iErr = 1;
                 break;
                 }
@@ -121,7 +182,7 @@ int main (int nArg, const char *psArg[])
                 else if ((! bIgn) && ( iBuff[7] != iFamily ))
                     {
                     fprintf (stderr, "Inconsistent family definition in \"%s\": 0x%08X 0x%08X\n",
-                        psArg[iArg], iFamily, iBuff[7]);
+                        psArg[pfi->iArg], iFamily, iBuff[7]);
                     iErr = 1;
                     break;
                     }
@@ -129,12 +190,11 @@ int main (int nArg, const char *psArg[])
             }
         if ( iBuff[3] % nBlkSize != 0 )
             {
-            fprintf (stderr, "Start address is not a multiple of block size in \"%s\"\n", psArg[iArg]);
+            fprintf (stderr, "Start address is not a multiple of block size in \"%s\"\n", psArg[pfi->iArg]);
             iErr = 1;
             break;
             }
-        pfs[iArg-nFIn] = pfi;
-        pfi->iArg     = iArg;
+        pfs[i] = pfi;
         pfi->nBlocks  = iBuff[6];
         pfi->iAddrMin = iBuff[3];
         pfi->iAddrMax = iBuff[3] + iBuff[6] * iBuff[4];
@@ -147,7 +207,7 @@ int main (int nArg, const char *psArg[])
     while ( ! bSorted )
         {
         bSorted = true;
-        for (int i = 0; i < nArg - nFIn - 1; ++i)
+        for (int i = 0; i < nFIn - 1; ++i)
             {
             if ( pfs[i+1]->iAddrMin < pfs[i]->iAddrMin )
                 {
@@ -160,33 +220,33 @@ int main (int nArg, const char *psArg[])
         }
     uint32_t nBlocks = 0;
     pfs[0]->iAddrBegin = pfs[0]->iAddrMin & SECTOR_MASK;
-    for (int i = 0; i < nArg - nFIn - 1; ++i)
+    for (int i = 0; i < nFIn - 1; ++i)
         {
         if ( pfs[i+1]->iAddrMin < pfs[i]->iAddrMax )
             {
-            fprintf (stderr, "File \"%s\" overlaps file \"%s\".", psArg[pfs[i]->iArg], psArg[pfs[i+1]->iArg]);
+            fprintf (stderr, "File \"%s\" overlaps file \"%s\".\n", psArg[pfs[i+1]->iArg], psArg[pfs[i]->iArg]);
             return 1;
             }
         pfs[i]->iAddrEnd = (pfs[i]->iAddrMax + SECTOR_SIZE - 1) & SECTOR_MASK;
         pfs[i+1]->iAddrBegin = pfs[i+1]->iAddrMin & SECTOR_MASK;
-        if (pfs[i+1]->iAddrBegin < pfs[i]->iAddrEnd)
+        if (bEra || (pfs[i+1]->iAddrBegin < pfs[i]->iAddrEnd))
             {
             pfs[i]->iAddrEnd = pfs[i]->iAddrMax;
             pfs[i+1]->iAddrBegin = pfs[i]->iAddrMax;
             }
         nBlocks += (pfs[i]->iAddrEnd - pfs[i]->iAddrBegin) / nBlkSize;
         }
-    pfs[nArg - nFIn - 1]->iAddrEnd = (pfs[nArg - nFIn - 1]->iAddrMax + SECTOR_SIZE - 1) & SECTOR_MASK;
-    nBlocks += (pfs[nArg - nFIn - 1]->iAddrEnd - pfs[nArg - nFIn - 1]->iAddrBegin) / nBlkSize;
-    fOut = fopen (psArg[nFIn-1], "wb");
+    pfs[nFIn - 1]->iAddrEnd = (pfs[nFIn - 1]->iAddrMax + SECTOR_SIZE - 1) & SECTOR_MASK;
+    nBlocks += (pfs[nFIn - 1]->iAddrEnd - pfs[nFIn - 1]->iAddrBegin) / nBlkSize;
+    fOut = fopen (psOut, "wb");
     if ( fOut == NULL )
         {
-        fprintf (stderr, "Unable to open output file \"%s\"\n", psArg[1]);
+        fprintf (stderr, "Unable to open output file \"%s\"\n", psOut);
         return 1;
         }
     int iBlkOut = 0;
     int iArg = pfs[0]->iArg;
-    for (int i = 0; i < nArg - nFIn; ++i)
+    for (int i = 0; i < nFIn; ++i)
         {
         pfi = pfs[i];
         uint32_t iAddrMax = pfi->iAddrBegin;
@@ -196,7 +256,7 @@ int main (int nArg, const char *psArg[])
             iBuff[4] = nBlkSize;
             iBuff[6] = nBlocks;
             if ( iFamily == 0 ) iBuff[7] = nBlocks * nBlkSize;
-            memset (&iBuff[8], 0, nBlkSize);
+            memset (&iBuff[8], uPad, nBlkSize);
             for (int i = 0; i < nPad; ++i)
                 {
                 iBuff[3] = iAddrMax;
@@ -276,7 +336,7 @@ int main (int nArg, const char *psArg[])
             iBuff[4] = nBlkSize;
             iBuff[6] = nBlocks;
             if ( iFamily == 0 ) iBuff[7] = nBlocks * nBlkSize;
-            memset (&iBuff[8], 0, nBlkSize);
+            memset (&iBuff[8], uPad, nBlkSize);
             for (int i = 0; i < nPad; ++i)
                 {
                 iBuff[3] = iAddrMax;
@@ -293,7 +353,8 @@ int main (int nArg, const char *psArg[])
                 }
             }
         if ( iErr != 0 ) break;
-        printf ("%3d: 0x%08X - 0x%08X %s\n", i + 1, pfi->iAddrMin, pfi->iAddrMax, psArg[iArg]);
+        printf ("%3d: 0x%08X - 0x%08X (0x%08X - 0x%08X) %s\n", i + 1, pfi->iAddrMin, pfi->iAddrMax,
+            pfi->iAddrBegin, pfi->iAddrEnd, psArg[iArg]);
         }
     fclose (fOut);
     return iErr;
