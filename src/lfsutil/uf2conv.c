bdc1f7b276afbf4eb5dcc7f7b8c21b1bf8385027
Memotech Bill <64212746+Memotech-Bill@users.noreply.github.com>
2024-10-19 15:42:13 +0100

    Set UF2 family ID of filesystem_*.uf2 to match bbcbasic_*.uf2

--- src/lfsutil/uf2conv.c
+++ src/lfsutil/uf2conv.c
@@ -36,7 +36,7 @@ const char *psName;
 void error (const char *psErr)
     {
     fprintf (stderr, "%s", psErr);
-    fprintf (stderr, "USAGE: %s [-d ABSOLUTE|2040|DATA|2350[_ASM_S]|2350_RISCV|2350_ARM_NS] [-f <id>] file.bin file.uf2 [address]\n", psName);
+    fprintf (stderr, "USAGE: %s [-d ABSOLUTE|2040|DATA|2350[_ASM_S]|2350_RISCV|2350_ARM_NS] [-f <id>] [-m ref.uf2] file.bin file.uf2 [address]\n", psName);
     exit (1);
     }
 
@@ -62,6 +62,8 @@ uint32_t parse_address (const char *ps)
 int main(int argc, char** argv) {
     const char *psIn = NULL;
     const char *psOut = NULL;
+    const char *psRef = NULL;
+    FILE* f;
     uint32_t address = 0;
     uint32_t family = 0;
     int nArg = 0;
@@ -86,6 +88,10 @@ int main(int argc, char** argv) {
                 family = strtoul(argv[i], NULL, 0);
                 flag = '\0';
                 break;
+            case 'm':
+                psRef = argv[i];
+                flag = '\0';
+                break;
             default:
                 if (flag) error ("Unrecognised switch\n");
                 if (argv[i][0] == '-')
@@ -104,43 +110,73 @@ int main(int argc, char** argv) {
             }
         }
     if (psIn == NULL) error ("No input file specified\n");
-    if (psOut == NULL) error ("No output file speccified\n");
+    if (psOut == NULL) error ("No output file specified\n");
 
-    FILE* f = fopen(argv[1], "rb");
-    if (!f) {
-        fprintf(stderr, "No such file: %s\n", argv[1]);
+    UF2_Block bl;
+    memset(&bl, 0, sizeof(bl));
+
+    if (psRef != NULL)
+        {
+        f = fopen (psRef, "rb");
+        if (!f)
+            {
+            fprintf(stderr, "No reference file: %s\n", psRef);
+            return 1;
+            }
+        if (fread (&bl, sizeof (uint32_t), 8, f) != 8)
+            {
+            fprintf(stderr, "Error reading reference file: %s\n", psRef);
+            fclose (f);
+            return 1;
+            }
+        fclose (f);
+        if (bl.flags & UF2_FLAG_FAMILY_ID_PRESENT) family = bl.fileSize;
+        }
+    else
+        {
+        bl.magicStart0 = UF2_MAGIC_START0;
+        bl.magicStart1 = UF2_MAGIC_START1;
+        bl.flags = family ? UF2_FLAG_FAMILY_ID_PRESENT : 0;
+        bl.fileSize = family;
+        }
+
+
+    f = fopen(psIn, "rb");
+    if (!f)
+        {
+        fprintf(stderr, "No such file: %s\n", psIn);
         return 1;
-    }
+        }
 
     fseek(f, 0L, SEEK_END);
     uint32_t sz = ftell(f);
     fseek(f, 0L, SEEK_SET);
 
-    const char* outname = argc > 2 ? argv[2] : "flash.uf2";
-
-    FILE* fout = fopen(outname, "wb");
-
-    UF2_Block bl;
-    memset(&bl, 0, sizeof(bl));
-
-    bl.magicStart0 = UF2_MAGIC_START0;
-    bl.magicStart1 = UF2_MAGIC_START1;
-    bl.magicEnd = UF2_MAGIC_END;
-    bl.flags = family ? UF2_FLAG_FAMILY_ID_PRESENT : 0;
-    bl.fileSize = family;
     bl.targetAddr = address;
     bl.numBlocks = (sz + 255) / 256;
     bl.payloadSize = 256;
+    bl.magicEnd = UF2_MAGIC_END;
+
+    FILE* fout = fopen(psOut, "wb");
+    if (!fout)
+        {
+        fprintf(stderr, "Unable to create output file: %s\n", psOut);
+        return 1;
+        }
+    
     int numbl = 0;
-    while (fread(bl.data, 1, bl.payloadSize, f)) {
+    while (fread(bl.data, 1, bl.payloadSize, f))
+        {
         bl.blockNo = numbl++;
         fwrite(&bl, 1, sizeof(bl), fout);
         bl.targetAddr += bl.payloadSize;
         // clear for next iteration, in case we get a short read
         memset(bl.data, 0, sizeof(bl.data));
-    }
+        }
     fclose(fout);
     fclose(f);
-    printf("Wrote %d blocks to %s\n", numbl, outname);
+    printf("Wrote %d blocks to \"%s\" from \"%s\", load address = 0x%08X", numbl, psOut, psIn, address);
+    if (family) printf (", family ID = 0x%08X", family);
+    printf ("\n");
     return 0;
 }
