0c31aa56511e9e7309027d3ac40ce5cdca577ce5
Memotech Bill <64212746+Memotech-Bill@users.noreply.github.com>
2024-11-25 15:59:16 +0000

    Revise board definitions for pico-sdk v2.1

--- boards/pico2+w.h
+++ boards/pico2+w.h
@@ -3,12 +3,12 @@
 //       SHOULD ONLY CONSIST OF PREPROCESSOR DIRECTIVES
 // -----------------------------------------------------
 
-// This header may be included by other board headers as "boards/pico+w.h"
-#ifndef _BOARDS_PICO_PW_H
-#define _BOARDS_PICO_PW_H
+// This header may be included by other board headers as "boards/pico2+w.h"
+#ifndef _BOARDS_PICO2_PW_H
+#define _BOARDS_PICO2_PW_H
 
 // For board detection
-#define RASPBERRYPI_PICO_PW
+#define RASPBERRYPI_PICO2_PW
 
 // --- LED ---
 #ifndef PICO_DEFAULT_LED_PIN
@@ -16,14 +16,6 @@
 #endif
 // no PICO_DEFAULT_WS2812_PIN
 
-#ifndef PICO_RP2040_B0_SUPPORTED
-#define PICO_RP2040_B0_SUPPORTED 1
-#endif
-
-#ifndef PICO_RP2040_B1_SUPPORTED
-#define PICO_RP2040_B1_SUPPORTED 1
-#endif
-
-#include <boards/pico_w.h>
+#include <boards/pico2_w.h>
 
 #endif
